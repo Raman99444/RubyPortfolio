bd64ba3cffa16d39be7988e5916850119502db67
ramachandran.p@introhive.com <ramachandran.p@introhive.com>
2020-12-15 19:47:24 +0530

    portfolio changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :blogs
-  get 'pages/home'
+  
   get 'pages/about'
   get 'pages/contact'
   resources :guides
   resources :guids
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "pages#home"
 end
