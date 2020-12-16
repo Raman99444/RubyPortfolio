1748f4fc773e2450308cd0da17a6c19f3a8cdd5b
ramachandran.p@introhive.com <ramachandran.p@introhive.com>
2020-12-16 11:38:28 +0530

    enum related changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, excep: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
   
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :guides
   resources :guids
   
-  root to: "pages#home"
+  root to: 'pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_075834) do
+ActiveRecord::Schema.define(version: 2020_12_16_054916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_075834) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
   end
 
   create_table "guides", force: :cascade do |t|
